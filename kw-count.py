59e32d81ddbbe9471641aeddb4aa2f7bb22361dc
Aurel Wisse <4907846+awisse@users.noreply.github.com>
2023-12-11 09:50:19 -0500

    Refined identification of keywords plus ...
    * Option to print all keywords
    * Option to show only keywords matching a regex pattern

--- kw-count.py
+++ kw-count.py
@@ -16,11 +16,25 @@ import re
 
 syn_match = re.compile("syn(tax)?\s+keyword")
 match_match = re.compile("syn(tax)?\s+match")
+kw_match = re.compile(r"(?<=[<(|])[-A-Za-z0-9_]{2,}")
 bs_match = re.compile(r"\s+\\")
 del_match = re.compile(r"syn(tax)?\s+match\s+\w+")
-FORMAT_STR = ["        {fn}:{line}  {comment}",
-          "{kw:20s} {line:3d} {comment:7s} {fn}"]
+TB_HEADER = "keyword              Line Match   Filename"
+FORMAT_STR = ["        {fn}: {line}  {comment}",
+              "{kw:23s} {line:4d} {comment:7s} {fn}"]
 MATCH_STR = "(match)"
+
+class KwLen:
+    """
+    Helper class to track longest keyword
+    """
+    length = 0
+    keyword = ''
+
+    def update(self, kw):
+        l = len(kw)
+        if l > self.length:
+            self.length, self.keyword = l, kw
 
 def parse_keywords(fn, keywords):
     """
@@ -28,13 +42,19 @@ def parse_keywords(fn, keywords):
     """
     with open(fn, 'r') as f:
         for lineno, line in enumerate(f, start=1):
+            # Lines starting with "syn keyword"
             if syn_match.match(line):
-                kw_start = 3
+                kw_list = re.split('\s+', line.strip())[3:]
+            # Lines with "\" as first non whitespace character
             elif bs_match.match(line):
-                kw_start = 1
+                kw_list = re.split('\s+', line.strip())[1:]
+            # Lines starting with "syn match"
+            elif match_match.match(line):
+                # Assumption: match expression contains no whitespace
+                # Everything after position 3 is a syntax argument
+                kw_list = kw_match.findall(line.split()[3])
             else:
                 continue
-            kw_list = re.split('\s+', line.strip())[kw_start:]
             for kw in kw_list:
                 keywords.setdefault(kw, []).append((fn, lineno))
 
@@ -42,9 +62,11 @@ def parse_matches(fn, keywords, matches):
     """
     Find lines with "syntax match" that contain keywords.
     """
-    def find_keyword(line):
-        for kw in keywords:
-            if re.search(r'\b{}\b'.format(kw), line):
+    def find_keyword():
+        for kw, positions in keywords.items():
+            # Don't count matches that were already counted in keywords
+            if re.search(r'[^-]\b{}\b[^-]'.format(kw), line) \
+                    and not (fn, lineno) in positions:
                 return kw
         return ''
 
@@ -53,7 +75,7 @@ def parse_matches(fn, keywords, matches):
             if match_match.match(line):
                 # Exclude "syn(tax) match [word]" from match"
                 line = del_match.sub('', line)
-                kw = find_keyword(line)
+                kw = find_keyword()
                 if kw:
                     matches.setdefault(kw, []).append((fn, lineno))
 
@@ -74,12 +96,27 @@ def parse_files(files, keywords):
     for fn in files:
         parse_keywords(fn, keywords)
 
-def print_results(keywords, matches, table=False):
+def print_results(keywords, matches, pattern='.*', table=False, pr_all=False):
     """
     Print information for duplicate entries.
     """
+    # Multiple matches must appear at least twice.
+    # With the option `pr_all`, appearing once is fine
+    multi = 0 if pr_all else 1
+    # Find longest keyword
+    key_len = KwLen()
+    # Count multiple defined keywords
+    multi_count = 0
+    if table:
+        print(TB_HEADER)
     for kw, context in keywords.items():
-        if (len(context) > 1) or (kw in matches):
+        key_len.update(kw)
+        found_kwds = len(context)
+        if (found_kwds > multi) or (kw in matches):
+            if found_kwds > 1:
+                multi_count += 1
+            if not re.match(pattern, kw):
+                continue
             if not table:
                 print(f"{kw}: ")
             for filename, linenumber in context:
@@ -91,6 +128,15 @@ def print_results(keywords, matches, table=False):
                     print(FORMAT_STR[table]
                           .format(fn=filename, line=linenumber,
                                   kw=kw, comment=MATCH_STR))
+
+    if not table:
+        # Longest keyword
+        print(f"Longest keyword      : {key_len.keyword} "
+              f"({key_len.length} characters)")
+        # Number of keys
+        print(f"Number of keywords   : {len(keywords)}")
+        # Number of keywords defined in multiple contexts
+        print(f"Multiple defined kwds: {multi_count}")
 
 def prepare_options():
     """
@@ -115,6 +161,16 @@ def prepare_options():
                         default=False,
                         help="Print output in table format")
 
+    parser.add_argument('--regex', '-r',
+                        default='.*',
+                        help="Print keywords matching regex pattern")
+
+    parser.add_argument('--all', '-a',
+                        action='store_true',
+                        default=False,
+                        help=("Print all found keywords with line number "
+                              "and filename"))
+
     return parser
 
 def main():
@@ -130,7 +186,7 @@ def main():
     else:
         matches = {}
 
-    print_results(keywords, matches, args.table)
+    print_results(keywords, matches, args.regex, args.table, args.all)
 
 if __name__ == '__main__':
     main()
