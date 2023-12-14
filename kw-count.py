c76d0c8e06555220512cdb4929c479725549af98
Aurel Wisse <4907846+awisse@users.noreply.github.com>
2023-12-14 08:55:51 -0500

    Refactored and improved output
    * Simplified parsing and duplicate detection.
    * Improved output format

--- kw-count.py
+++ kw-count.py
@@ -13,16 +13,19 @@ Count number of definitions for each keyword definition
 
 import argparse
 import re
+from collections import namedtuple
 
-syn_match = re.compile("syn(tax)?\s+keyword")
-match_match = re.compile("syn(tax)?\s+match")
-color_match = re.compile(r"hi\s+link\s+(\w+)\s+(\w+)")
-kw_match = re.compile(r"(?<=[<(|])[-A-Za-z0-9_]{2,}")
-bs_match = re.compile(r"\s+\\")
-del_match = re.compile(r"syn(tax)?\s+match\s+\w+")
-TB_HEADER = "keyword              Line Match   Filename"
-FORMAT_STR = ["        {fn}: {line}  {comment}",
-              "{kw:23s} {line:4d} {comment:7s} {fn}"]
+syn_kw_pat = re.compile("syn(tax)?\s+keyword\s+\w+")
+syn_match_pat = re.compile("syn(tax)?\s+match\s+\w+")
+hilink_pat = re.compile(r"hi\s+link\s+(\w+)\s+(\w+)")
+hicolor_pat = re.compile(r"hi\s+(\w+)\s+ctermfg=")
+match_kw_pat = re.compile(r"(?<=[<(|])[-A-Za-z0-9_]{2,}")
+bs_pat = re.compile(r"\s+\\")
+KW_WIDTH = 25 # Width of keyword column in table
+SYN_WIDTH = 8 # Width of syn column in table
+TB_HEADER = "Keyword                   Line syn     Filename"
+FORMAT_STR = ["        {syn_type:%ds} {fn}: {line}  " % SYN_WIDTH,
+              "{kw:%ds} {line:4d} {syn_type:%ds} {fn}" % (KW_WIDTH, SYN_WIDTH)]
 MATCH_STR = "(match)"
 
 class KwLen:
@@ -37,105 +40,92 @@ class KwLen:
         if l > self.length:
             self.length, self.keyword = l, kw
 
-def parse_keywords(fn, keywords):
-    """
-    Parse one file and update the keywords dictionary.
-    """
-    with open(fn, 'r') as f:
-        for lineno, line in enumerate(f, start=1):
-            # Lines starting with "syn keyword"
-            if syn_match.match(line):
-                kw_list = re.split('\s+', line.strip())[3:]
-            # Lines with "\" as first non whitespace character
-            elif bs_match.match(line):
-                kw_list = re.split('\s+', line.strip())[1:]
-            # Lines starting with "syn match"
-            elif match_match.match(line):
-                # Assumption: match expression contains no whitespace
-                # Everything after position 3 is a syntax argument
-                kw_list = kw_match.findall(line.split()[3])
-            else:
-                continue
-            for kw in kw_list:
-                keywords.setdefault(kw, []).append((fn, lineno))
+"""
+All information related to a keyword.
+1. Filename where it was found.
+2. Linenumber in that filename.
+3. Found in a `syn keyword` line or a `syn match` line.
+"""
+KwInfo = namedtuple('KwInfo', 'filename linenumber syn')
 
-def parse_matches(fn, keywords, matches):
+def parse_keywords(f, filename, keywords):
     """
-    Find lines with "syntax match" that contain keywords.
+    Parse file object f and update the keywords dictionary.
     """
-    def find_keyword():
-        for kw, positions in keywords.items():
-            # Don't count matches that were already counted in keywords
-            if re.search(r'[^-]\b{}\b[^-]'.format(kw), line) \
-                    and not (fn, lineno) in positions:
-                return kw
-        return ''
+    def update_keywords(syn='keyword'):
+        for kw in kw_list:
+            keywords.setdefault(kw, []).append(KwInfo(filename, lineno, syn))
 
-    with open(fn, 'r') as f:
-        for lineno, line in enumerate(f, start=1):
-            if match_match.match(line):
-                # Exclude "syn(tax) match [word]" from match"
-                line = del_match.sub('', line)
-                kw = find_keyword()
-                if kw:
-                    matches.setdefault(kw, []).append((fn, lineno))
-
-    return matches
+    for lineno, line in enumerate(f, start=1):
+        # Lines starting with "syn keyword"
+        if syn_kw_pat.match(line):
+            kw_list = re.split('\s+', line.strip())[3:]
+            update_keywords()
+        # Lines with "\" as first non whitespace character
+        elif bs_pat.match(line):
+            kw_list = re.split('\s+', line.strip())[1:]
+            update_keywords()
+        # Lines starting with "syn match"
+        elif syn_match_pat.match(line):
+            # Assumption: match expression contains no whitespace
+            # Everything after position 3 is a syntax argument
+            kw_list = match_kw_pat.findall(line.split()[3])
+            update_keywords('match')
 
 def parse_files(files, keywords):
     """
-    1. Create the dictionary.
-    2. Parse each file and update the dictionary.
+    Parse each file with the parse function and update the dictionary.
+    `args` are passed to the `parse_function`.
     """
-    if keywords:
-        matches = {}
-        for fn in files:
-            parse_matches(fn, keywords, matches)
+    for filename in files:
+        with open(filename, 'r') as f:
+            parse_keywords(f, filename, keywords)
 
-        return matches
+def parse_colors(fn, hlgroups, color_defs):
+    """
+    Parse highlight groups and color definitions into the two
+    dictionaries `hlgroups` and `color_defs`.
+    """
 
-    for fn in files:
-        parse_keywords(fn, keywords)
-
-def parse_colors(files, color_defs):
+def parse_colors(files, hlgroups, color_defs):
     """
     Parse color groups and highlight group definitions.
     Identify highlight groups to be defined.
     Identify unused defined highlight groups.
     """
+    for fn in files:
+        parse_colors(fn, hlgroups, color_defs)
 
-def print_results(keywords, matches, pattern='.*', table=False, pr_all=False):
+
+def print_results(keywords, pattern='.*', table=False, print_all=False):
     """
     Print information for duplicate entries.
     """
     # Multiple matches must appear at least twice.
-    # With the option `pr_all`, appearing once is fine
-    multi = 0 if pr_all else 1
+    # With the option `print_all`, appearing once is fine
+    multi = 0 if print_all else 1
     # Find longest keyword
     key_len = KwLen()
     # Count multiple defined keywords
     multi_count = 0
+    # Keyword selection pattern
+    kw_sel = re.compile(pattern)
     if table:
         print(TB_HEADER)
     for kw, context in keywords.items():
         key_len.update(kw)
         found_kwds = len(context)
-        if (found_kwds > multi) or (kw in matches):
+        if (found_kwds > multi):
             if found_kwds > 1:
                 multi_count += 1
-            if not re.match(pattern, kw):
+            if not kw_sel.match(kw):
                 continue
             if not table:
                 print(f"{kw}: ")
-            for filename, linenumber in context:
+            for c in context:
                 print(FORMAT_STR[table]
-                      .format(fn=filename, line=linenumber,
-                              kw=kw, comment=''))
-            if kw in matches:
-                for filename, linenumber in matches[kw]:
-                    print(FORMAT_STR[table]
-                          .format(fn=filename, line=linenumber,
-                                  kw=kw, comment=MATCH_STR))
+                      .format(fn=c.filename, line=c.linenumber,
+                              kw=kw[:KW_WIDTH], syn_type=c.syn))
 
     if not table:
         # Longest keyword
@@ -154,20 +144,13 @@ def prepare_options():
             description="Identify duplicate keywords in VIM Syntax file")
 
     # One or more positional arguments: filenames to be parsed
-    parser.add_argument('file', nargs='+',
+    parser.add_argument('files', nargs='+',
                         help="One or more files to parse")
-
-    # Check if keywords appear in matches. These keywords will be
-    # highlighted with the "keyword" color rule given the precedence
-    # of syntax highlighting in VIM. Print the lines
-    # With keywords in matches.
-    parser.add_argument('--match', '-m', action='store_true',
-                        default=False,
-                        help="Print keywords appearing in matches")
 
     parser.add_argument('--table', '-t', action='store_true',
                         default=False,
-                        help="Print output in table format")
+                        help=("Print output in table format "
+                              "(fixed width columns)"))
 
     parser.add_argument('--regex', '-r',
                         default='.*',
@@ -176,8 +159,8 @@ def prepare_options():
     parser.add_argument('--all', '-a',
                         action='store_true',
                         default=False,
-                        help=("Print all found keywords with line number "
-                              "and filename"))
+                        help=("Print all keywords with filename and "
+                              "and line number"))
 
     parser.add_argument('--colors', '-c',
                         default='store_true',
@@ -192,14 +175,12 @@ def main():
     args = parser.parse_args()
 
     keywords = {}
-    matches = parse_files(args.file, keywords)
+    parse_files(args.files, keywords)
 
-    if args.match:
-        matches = parse_files(args.file, keywords)
-    else:
-        matches = {}
+    print_results(keywords, args.regex, args.table, args.all)
 
-    print_results(keywords, matches, args.regex, args.table, args.all)
+    if args.colors:
+        hlgroups, color_defs = {}, {}
 
 if __name__ == '__main__':
     main()
